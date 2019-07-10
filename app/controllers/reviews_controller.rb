028a48ae9cc24d4d62e57279d8b5add1b2c82803
Hasan Karli <karlihasann@gmail.com>
2019-07-10 14:57:48 +0300

    Edit and Destroy reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,52 @@
 class ReviewsController < ApplicationController
-	before_action :find_play, only: [:new, :create]
+	before_action :find_play
+	before_action :find_review, only: [:edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :edit]
 
 	def new
-	 @review = Review.new	
+		@review = Review.new
 	end
 
-  def create
-	 	
-	 @review = Review.new()
-	 @review.play_id = @play_id
-	 @review.user_id = current_user.id
+	def create
+		@review = Review.new(review_params)
+		@review.play_id = @play.id
+		@review.user_id = current_user.id
 
-	   if @review.save
-		 	 redirect_to play_path(@play)
-		 else
-		 	 render 'new'
-		 end		
+		if @review.save
+			redirect_to play_path(@play)
+		else
+			render 'new'
+		end
 	end
 
-	private
-	 	def review_params
-	 		params.require(:review).permit(:rating, :comment)
-	 	end
+	def edit
+	end
 
-	 	def find_play
+	def update	
+		if @review.update(review_params)
+			redirect_to play_path(@play)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@review.destroy
+		redirect_to play_path(@play)
+	end
+
+	private 
+
+		def review_params
+			params.require(:review).permit(:rating, :comment)
+		end
+
+		def find_play
 			@play = Play.find(params[:play_id])
-	 	end
+		end
+
+		def find_review
+			@review = Review.find(params[:id])
+		end
+
 end
