b8254700e3aab435212d71219624f8bc22ef8089
Hasan Karli <karlihasann@gmail.com>
2019-07-11 09:53:30 +0300

    Add Category Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :plays do
   	resources :reviews
-  end	
+  end
+  resources :categories
   root "plays#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
